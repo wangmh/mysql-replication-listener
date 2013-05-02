1839015bc6d8b9c442ff933b575ffbb4d1a09dfd
ideal <idealities@gmail.com>
2013-05-02 10:16:44 +0800

    modify format

--- src/tcp_driver.cpp
+++ src/tcp_driver.cpp
@@ -17,10 +17,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 02110-1301  USA
 */
+
 #include "binlog_api.h"
+
 #include <iostream>
 #include <boost/bind.hpp>
-#include "tcp_driver.h"
 
 #include <fstream>
 #include <time.h>
@@ -33,6 +34,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 #include <openssl/evp.h>
 #include <openssl/rand.h>
 
+#include "tcp_driver.h"
 #include "protocol.h"
 #include "binlog_event.h"
 #include "rowset.h"
